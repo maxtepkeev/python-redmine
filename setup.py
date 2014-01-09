40e637901ce70a2320f53affa4e2a8dc8548f605
Max Tepkeev <tepkeev@gmail.com>
2014-01-09 17:48:36 +0400

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='python-redmine',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['redmine'],
     url='https://github.com/maxtepkeev/python-redmine',
     license=open('LICENSE').read(),
     author='Max Tepkeev',
