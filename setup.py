af101827f68bd25ecb85b64788e212f9edf576ef
Max Tepkeev <tepkeev@gmail.com>
2014-03-23 12:58:28 +0400

    all is well under Python 3.4

--- setup.py
+++ setup.py
@@ -29,5 +29,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
