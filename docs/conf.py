896f20e5b75791b65dbda3c56e44bf6ce1e460e9
Max Tepkeev <tepkeev@gmail.com>
2014-09-12 10:54:33 +0400

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -182,22 +182,21 @@ htmlhelp_basename = 'PythonRedminedoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'PythonRedmine.tex', u'Python Redmine Documentation',
-   u'Max Tepkeev', 'manual'),
+    ('index', 'PythonRedmine.tex', u'Python Redmine Documentation', u'Max Tepkeev', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +225,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pythonredmine', u'Python Redmine Documentation',
-     [u'Max Tepkeev'], 1)
+    ('index', 'pythonredmine', u'Python Redmine Documentation', [u'Max Tepkeev'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,9 +238,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PythonRedmine', u'Python Redmine Documentation',
-   u'Max Tepkeev', 'PythonRedmine', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'PythonRedmine', u'Python Redmine Documentation', u'Max Tepkeev', 'PythonRedmine', 'Desc', 'Misc'),
 ]
 
 # Documents to append as an appendix to all manuals.
