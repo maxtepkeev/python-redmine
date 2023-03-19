a37cb21715f41f54ffed04fbe8b5ab2734da8460
Max Tepkeev <tepkeev@gmail.com>
2023-03-19 15:45:00 +0300

    switch to latest sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.jquery']
 
 # How to sort documented members
 autodoc_member_order = 'bysource'
